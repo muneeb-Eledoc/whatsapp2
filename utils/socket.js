467e5d77e5206440334aace3800fd90583e6dc1e
muneeb-Eledoc <muneebjs.css@gmail.com>
2022-07-20 18:59:53 +0500

    mistake

--- utils/socket.js
+++ utils/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-// export const socket = io('https://whatsapp2--0.herokuapp.com/');
-export const socket = io('ws://localhost:1337');
+export const socket = io('https://whatsapp2--0.herokuapp.com/');
+// export const socket = io('ws://localhost:1337');
